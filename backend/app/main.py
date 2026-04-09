2247dd50b3066a52361849c7fb06a5b1b8054254
Mohammed Abdus Samad <abdussamad8886@gmail.com>
2026-04-09 22:29:32 +0530

    Deployment: Configure production API URL and CORS policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
